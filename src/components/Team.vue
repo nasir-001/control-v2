fa08b1f196ec647e7930193b1d5a9745c7926ba4
nasir-001 <nasirlawal001@gmail.com>
2023-03-16 16:00:15 +0100

    designed meat our team

---
+++ src/components/Team.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="tw-mt-24">
+    <div>
+      <div class="title tw-text-3xl tw-text-center">Meet Our Team</div>
+      <div class="row tw-justify-center tw-mt-8">
+        <div
+          class="tw-w-64 tw-h-56 tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl"
+        >
+          <div class="row tw-justify-center">
+            <q-img
+              src="~assets/img/bashir.png"
+              class="tw-w-24 tw-h-24 tw-rounded-full tw-mt-10"
+            />
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Bashir Isah Dodo
+          </div>
+        </div>
+        <div
+          class="tw-w-64 tw-h-56 tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl"
+        >
+          <div class="row tw-justify-center">
+            <q-img
+              src="~assets/img/nasir.png"
+              class="tw-w-24 tw-h-24 tw-rounded-full tw-mt-10"
+            />
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Nasir Ibrahim Abba
+          </div>
+        </div>
+        <div
+          class="tw-w-64 tw-h-56 tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl"
+        >
+          <div class="row tw-justify-center">
+            <q-img
+              src="~assets/img/nasir.png"
+              class="tw-w-24 tw-h-24 tw-rounded-full tw-mt-10"
+            />
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Dahir Muhammad Dahir
+          </div>
+        </div>
+        <div
+          class="tw-w-64 tw-h-56 tw-border-2 tw-mx-2 tw-border-primary-bg-color tw-rounded-2xl"
+        >
+          <div class="row tw-justify-center">
+            <q-img
+              src="~assets/img/nasir.png"
+              class="tw-w-24 tw-h-24 tw-rounded-full tw-mt-10"
+            />
+          </div>
+          <div class="tw-text-center tw-font-medium tw-text-base tw-pt-2">
+            Nasir Ibrahim Abba
+          </div>
+        </div>
+      </div>
+      <div class="row tw-justify-center tw-mt-8">
+        <q-btn
+          outline
+          no-caps
+          class="tw-w-36 tw-rounded-full"
+          label="Join Our Team"
+          @click="onClick"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style scoped>
+.title {
+  background: linear-gradient(to right, #fd8a45 0%, #0b1e4f 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,6 +5,7 @@
     <note></note>
     <mission></mission>
     <bot></bot>
+    <team></team>
   </div>
 </template>
 
@@ -15,9 +16,10 @@ import Hero from "components/Hero.vue";
 import Note from "components/Note.vue";
 import Mission from "components/Mission.vue";
 import Bot from "components/Bot.vue";
+import Team from "components/Team.vue";
 
 export default defineComponent({
-  components: { Introduction, Hero, Note, Mission, Bot },
+  components: { Introduction, Hero, Note, Mission, Bot, Team },
   name: "IndexPage",
 });
 </script>
